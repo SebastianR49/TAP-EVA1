6a5fc1e3e01ea53c1c356b689d577002326fe0a6
SebastianR49 <118313737+SebastianR49@users.noreply.github.com>
2023-09-13 02:10:44 -0500

    Add files via upload

---
+++ EVA1_7_STACK/src/Stack.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+/**
+ *
+ * @author sebas
+ */
+public class Stack {
+
+  /**
+   * @param args the command line arguments
+   */
+  public static void main(String[] args) {
+    System.out.println("inicia main");
+    A();
+    System.out.println("fin main");
+    
+    // TODO code application logic here
+  }
+  
+   public static void A(){
+    System.out.println("inicia a");
+    B();
+    System.out.println("fin a");
+  }
+  
+  public static void B(){
+    System.out.println("inicia b");
+    System.out.println("fin b");
+    
+  }
+  
+}
